c43403c080bf578c22e2697ef278c532938190c6
Kanv Khare <kanvkhurana@hotmail.com>
2024-10-12 00:07:21 -0400

    Added base streamlit UI

--- project/main.py
+++ project/main.py
@@ -1,9 +1,10 @@
-import gradio as gr
+import streamlit as st
 from git import Repo
 
 
 def hello_world(name):
     return f"Hello, {name}!"
+
 
 def clone_repository(repo_url):
     # Clone the repo with url repo_url into the cloned_repos directory
@@ -17,11 +18,27 @@ def clone_repository(repo_url):
         status = f"An error occurred while cloning the repository: {e}"
     return status
 
-# interface = gr.Interface(fn=hello_world, inputs="textbox", outputs="text")
-interface = gr.Interface(fn=clone_repository, inputs="textbox", outputs="text")
 
-interface.launch()
+def main():
+    """
+    Main function for the Streamlit app.
+    """
+    st.set_page_config(page_title="CodeRECAP", layout="wide")
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    st.title("CodeRECAP")
+    name = st.text_input("Enter your name:")
+    if name:
+        st.markdown(f"Hello, {name}!")
 
+
+if __name__ == "__main__":
+    main()
 
 # Example usage
 # repo_url = "https://github.com/kanvk/CodeRECAP.git"
