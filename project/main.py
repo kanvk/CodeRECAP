74ea693df3116bd7f36736963ff021a303f07a82
Adithya Kiran <adithyakiran1999@gmail.com>
2024-10-17 13:51:06 -0400

    Updated Clone Logic

--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from git import Repo
+import os
 
 
 def hello_world(name):
@@ -8,12 +9,19 @@ def hello_world(name):
 
 def clone_repository(repo_url):
     # Clone the repo with url repo_url into the cloned_repos directory
-    # TODO: Check if repo exists and run pull instead of clone if it exists
+    # If repo exists, run git pull instead to update it
     repo_name = repo_url.split("/")[-1][:-4]
     clone_dir = f"./cloned_repos/{repo_name}"
     try:
-        Repo.clone_from(repo_url, clone_dir)
-        status = f"Repository cloned successfully into {clone_dir}"
+        if os.path.exists(clone_dir):
+            # If the directory exists, perform git pull
+            status = f"Directory {clone_dir} already exists. Pulling latest changes..."
+            repo = Repo(clone_dir)
+            repo.remotes.origin.pull()
+            status+=" Repository updated successfully."
+        else:
+            Repo.clone_from(repo_url, clone_dir)
+            status = f"Repository cloned successfully into {clone_dir}"
     except Exception as e:
         status = f"An error occurred while cloning the repository: {e}"
     return status
@@ -32,13 +40,13 @@ def main():
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     st.title("CodeRECAP")
-    name = st.text_input("Enter your name:")
-    if name:
-        st.markdown(f"Hello, {name}!")
+    url = st.text_input("Enter the URL to the git repo to be analysed:")
+    if url:
+        st.markdown(f"{clone_repository(url)}")
+
 
 
 if __name__ == "__main__":
     main()
 
-# Example usage
-# repo_url = "https://github.com/kanvk/CodeRECAP.git"
+# Example usage: Input this URL in the text box: "https://github.com/kanvk/CodeRECAP.git"
