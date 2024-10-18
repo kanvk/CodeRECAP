012b2e7f561d3dc6b413aea2f0e962d0f076dcde
smansou2 <smansou2@ncsu.edu>
2024-10-18 00:20:19 -0400

    Putting AST analysis inegrated to cloned repository

--- project/storeFunctions.py
+++ project/storeFunctions.py
@@ -2,99 +2,99 @@ import sqlite3
 import os
 
 def create_database(db_name, path=os.getcwd()):
-    conn = sqlite3.connect(path+'/'+db_name+'.db')
+    conn = sqlite3.connect(os.path.join(path, f"{db_name}.db"))
     cursor = conn.cursor()
     
-    # Create table
+    # Create tables to store functions and arguments
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS functions (
-        name TEXT PRIMARY KEY NOT NULL,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
         start_line INTEGER,
-        end_line INTEGER
+        end_line INTEGER,
+        file_path TEXT
     );
     ''')
 
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS arguments (
-        function_name TEXT NOT NULL,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        function_id INTEGER NOT NULL,
         argument_type TEXT NOT NULL,
         name TEXT NOT NULL,
-        default_value TEXT,
-        FOREIGN KEY (function_name) REFERENCES functions (name) ON DELETE CASCADE
+        FOREIGN KEY (function_id) REFERENCES functions (id) ON DELETE CASCADE
     );
     ''')
 
     conn.commit()
-
     conn.close()
 
 def insert_function_data(functions_info, db_name, path=os.getcwd()):
-    conn = sqlite3.connect(path+'/'+db_name+'.db')
+    conn = sqlite3.connect(os.path.join(path, f"{db_name}.db"))
     cursor = conn.cursor()
 
     for function_info in functions_info:
         # Insert the function data
         cursor.execute(
             '''
-            INSERT INTO functions (name, start_line, end_line)
-            VALUES (?, ?, ?)
+            INSERT INTO functions (name, start_line, end_line, file_path)
+            VALUES (?, ?, ?, ?)
             ''', 
-            (function_info['name'], function_info['start_line'], function_info['end_line'])
+            (function_info['name'], function_info['start_line'], function_info['end_line'], function_info['file_path'])
         )
 
-        # Get the function ID of the inserted function
-        function_name = function_info['name']
+        # Get the ID of the last inserted function
+        function_id = cursor.lastrowid
 
-        # Insert function arguments 
+        # Insert function arguments (regular arguments)
         for arg in function_info['arguments']:
             cursor.execute(
                 '''
-                INSERT INTO arguments (function_name, argument_type, name)
+                INSERT INTO arguments (function_id, argument_type, name)
                 VALUES (?, ?, ?)
                 ''',
-                (function_name, 'argument', arg)
+                (function_id, 'argument', arg)
             )        
         
         # Insert positional arguments
         for arg in function_info['positional_arguments']:
             cursor.execute(
                 '''
-                INSERT INTO arguments (function_name, argument_type, name)
+                INSERT INTO arguments (function_id, argument_type, name)
                 VALUES (?, ?, ?)
                 ''',
-                (function_name, 'positional', arg)
+                (function_id, 'positional', arg)
             )
 
         # Insert variable-length positional arguments (if any)
         for arg in function_info['varlen_arguments']:
             cursor.execute(
                 '''
-                INSERT INTO arguments (function_name, argument_type, name)
+                INSERT INTO arguments (function_id, argument_type, name)
                 VALUES (?, ?, ?)
                 ''', 
-                (function_name, 'varlen', arg)
+                (function_id, 'varlen', arg)
             )
 
         # Insert keyword arguments
         for arg in function_info['keyword_arguments']:
             cursor.execute(
                 '''
-                INSERT INTO arguments (function_name, argument_type, name)
+                INSERT INTO arguments (function_id, argument_type, name)
                 VALUES (?, ?, ?)
                 ''', 
-                (function_name, 'keyword', arg)
+                (function_id, 'keyword', arg)
             )
 
         # Insert variable-length keyword arguments
         for arg in function_info['varlen_keyword_arguments']:
             cursor.execute(
                 '''
-                INSERT INTO arguments (function_name, argument_type, name)
+                INSERT INTO arguments (function_id, argument_type, name)
                 VALUES (?, ?, ?)
                 ''',
-                (function_name, 'varlen_keyword', arg)
+                (function_id, 'varlen_keyword', arg)
             )
         
     conn.commit()
-
     conn.close()
