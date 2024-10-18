80ad796eb1c8e833a0151e82827bd834cc7c2072
smansou2 <smansou2@ncsu.edu>
2024-10-18 00:34:16 -0400

    fix build error

--- project/main.py
+++ project/main.py
@@ -2,9 +2,9 @@ import streamlit as st
 from git import Repo
 import os
 import requests
-from project.identifyFunctions import get_function_info
+from identifyFunctions import get_function_info
 
-from project.storeFunctions import *
+from storeFunctions import *
 
 def update_streamlit_output_log(msg, append=True):
     if append:
